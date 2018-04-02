bca930f4a1950e9eb60f769b161e210cf7233537
Sirdigbot <sirdigbot@users.noreply.github.com>
2018-04-02 16:03:39 +1000

    Change to Cython Build

--- build.bat
+++ build.bat
@@ -1,2 +1,13 @@
 @echo off
-python -O -m PyInstaller --noconfirm --noconsole --noupx --onefile --clean --icon="textscramble.ico" --name TextScramble --distpath ./bin --workpath ./build scramble.pyw
+cython scramble.pyw --embed -3
+echo C File Generated with Cython Successfully. You will need to compile it separately.
+echo =================
+echo PLEASE NOTE:
+echo You will need to manually edit the generated C file and change
+echo    "int wmain(.."
+echo into
+echo    "int main(.."
+echo This is because you cannot use /subsystem:windows /ENTRY:mainCRTStartup
+echo if the main function is not actually called "main".
+echo =================
+pause
